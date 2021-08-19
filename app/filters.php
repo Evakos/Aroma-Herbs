709cb97add925be5ee14859a671a3e873d59c7a6
Evakos <info@evakos.io>
2021-08-19 19:19:41 +0300

    Third Commit

--- app/filters.php
+++ app/filters.php
@@ -10,7 +10,7 @@ namespace App;
  * Add "… Continued" to the excerpt.
  *
  * @return string
- */
+ */ssh
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
@@ -21,5 +21,8 @@ add_filter('acf/format_value/name=contact_form', 'eks_acf_format_value', 10, 3);
 
 //Remove P tags in Contact Form Field
 add_filter('wpcf7_autop_or_not', '__return_false');
+
+
+
 
 
