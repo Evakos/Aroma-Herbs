034e82e41ddb630db8599e92028ea60d8b694c8d
Evakos <info@evakos.io>
2021-09-26 17:34:14 +0300

    Animations

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -3,30 +3,6 @@
  */
 import "jquery";
 
-import AOS from "aos";
-
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: false, // class applied after initialization
-  animatedClassName: "animated", // class applied on animation
-  useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 0, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
-  easing: "ease", // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: true, // whether elements should animate out while scrolling past them
-  anchorPlacement: "top-top", // defines which position of the element regarding to window should trigger the animation
-});
 
 const getSingleProducts = document.querySelectorAll(".single-product");
 
@@ -106,7 +82,7 @@ function stickyHeader() {
   const floating = document.getElementById("floating");
 
 
-  if (sticky <= 0) {
+  if (sticky < 0) {
 
 
 //console.log("This is the sticky position:" + sticky)
