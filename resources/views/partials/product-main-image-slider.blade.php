2237da4165d4f46cabfb292995dbe092575f9355
Evakos <info@evakos.io>
2021-10-08 10:58:17 +0300

    Product Images Center

--- resources/views/partials/product-main-image-slider.blade.php
+++ resources/views/partials/product-main-image-slider.blade.php
@@ -7,7 +7,7 @@ if( $images ): ?>
         <?php foreach( $images as $image ): ?>
           <li class="splide__slide">
                 <a href="<?php echo esc_url($image['url']); ?>" class="product-image">
-                     <img src="<?php echo esc_url($image['sizes']['large']); ?>" class="w-[229px] sm:w-[340px] pt-32" alt="<?php echo esc_attr($image['alt']); ?>" />
+                     <img src="<?php echo esc_url($image['sizes']['large']); ?>" class="mx-auto w-[229px] sm:w-[340px] pt-44" alt="<?php echo esc_attr($image['alt']); ?>" />
                 </a>
                 <p><?php echo esc_html($image['caption']); ?></p>
             </li>
