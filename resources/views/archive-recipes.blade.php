5dc8ae3038003f5d39d16bc42613cf7fca5cc3b8
Evakos <info@evakos.io>
2021-10-08 10:07:00 +0300

    Padding Recipes

--- resources/views/archive-recipes.blade.php
+++ resources/views/archive-recipes.blade.php
@@ -157,7 +157,7 @@ else {echo 'row-span-1';}
 
 <a href="@permalink" class="absolute flex flex-col w-full bottom-0 xl:bottom-auto p-3 xl:p-0 xl:h-full justify-center items-center bg-light-green" id="twists-title">
 
-<p class="text-white text-center text-2xl mb-2 xl:mb-10">@title</p>
+<p class="text-white text-center p-10 text-2xl mb-2 xl:mb-10">@title</p>
 
 
 <p class="font-takhie text-xl text-white"><?php _e( 'Μάθε Περισσότερα', 'sage'); ?></p>
